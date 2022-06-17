9cd5314e16ddf9efdf3923f9667b5de4d553bbc3
Ali Dandan <alidandan17@outlook.com>
2022-06-17 15:03:48 +0300

    Comments added for documentation

--- models/estate_poperties.py
+++ models/estate_poperties.py
@@ -1,8 +1,11 @@
 from odoo import models, fields, api, exceptions
 
+# Create a new class for the estate_properties model
 class EstateProperties(models.Model):
+    # inherit the model from the estate.properties class
     _inherit = 'estate.properties'
 
+    #override the action_sell method to add a new field to the invoice
     def action_sell(self):
         res = self.env["account.journal"].search([("type", "=","sale")], limit=1)
         for prop in self:
